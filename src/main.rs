cf26b06abda8924dd0fcb1653175ada0488a16f6
nikolai <nikolaiquasarka@gmail.com>
2026-03-05 04:57:39 +0300

    Создана функция программы, функция получения размера грида и создание грида.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use std::io;
 
-use rectangle::{drawing::basic, figures};
+use rectangle::{
+    drawing::{self, basic, grid},
+    figures,
+};
 
-fn main() {}
+fn main() {
+    app();
+}
 
-fn app() {}
+fn app() {
+    let grid = loop {
+        let grid_size = get_grid_size();
+
+        let Ok(grid) = grid::Grid::from(grid_size) else {
+            eprint!("Try again");
+            continue;
+        };
+
+        break grid;
+    };
+}
 
 fn get_grid_size() -> basic::Size {
     println!("Enter grid size:");
 
     let width: u32 = loop {
+        println!("Enter width");
+
         let mut width: String = String::new();
 
         if let Err(e) = io::stdin().read_line(&mut width) {
@@ -17,8 +35,15 @@ fn get_grid_size() -> basic::Size {
             continue;
         }
 
-        match width.parse::<u32>() {
-            Ok(width) => break width,
+        match width.trim().parse::<u32>() {
+            Ok(width) => {
+                if width < 1 {
+                    eprintln!("Value below 1. Width should be 1 and higher");
+                    continue;
+                } else {
+                    break width;
+                }
+            }
             Err(_) => {
                 eprintln!("Wrong input. Try again");
                 continue;
@@ -27,6 +52,8 @@ fn get_grid_size() -> basic::Size {
     };
 
     let height: u32 = loop {
+        println!("Enter height");
+
         let mut height: String = String::new();
 
         if let Err(e) = io::stdin().read_line(&mut height) {
@@ -34,8 +61,15 @@ fn get_grid_size() -> basic::Size {
             continue;
         }
 
-        match height.parse::<u32>() {
-            Ok(height) => break height,
+        match height.trim().parse::<u32>() {
+            Ok(height) => {
+                if height < 1 {
+                    eprint!("Value below 1. Height shoud be 1 and higher");
+                    continue;
+                } else {
+                    break height;
+                }
+            }
             Err(_) => {
                 eprintln!("Wrong input. Try again");
                 continue;
