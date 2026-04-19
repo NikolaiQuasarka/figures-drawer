2766d5a5ad903bd7d513980c8593ca47c993325a
nikolai <nikolaiquasarka@gmail.com>
2026-04-19 19:17:02 +0300

    Заменил старые инпуты на новый метод

--- src/input/choose_figure/rectangle.rs
+++ src/input/choose_figure/rectangle.rs
@@ -1,44 +1,17 @@
-use crate::figures::rectangle;
+use crate::{figures::rectangle, input};
 use std::io;
 
 pub fn get_rectangle() -> rectangle::Rectangle {
-    let width = get_size("Введите ширину:");
-    let height = get_size("Введите высоту:");
+    let width = input::input(
+        "Введите ширину:",
+        "Ширина должна быть больше 0 и целым числом",
+        |&width| width > 0,
+    );
+    let height = input::input(
+        "Введите высоту:",
+        "Ширина должна быть больше 0 и целым числом",
+        |&height| height > 0,
+    );
 
     rectangle::Rectangle::from(width, height).expect("Не удалось создать прямоугольник")
-}
-
-fn get_size(prompt: &str) -> u32 {
-    let size = loop {
-        println!("{}", prompt);
-
-        let mut size = String::new();
-
-        if let Err(_) = io::stdin().read_line(&mut size) {
-            eprintln!("Ошибка ввода. Попробуте еще раз.");
-            continue;
-        }
-
-        let size = size.trim();
-
-        let size = match size.parse::<u32>() {
-            Ok(size) => {
-                if size < 1 {
-                    eprintln!("Размер должен быть не ниже 1. Нечетные числа не поддерживаются.");
-                    continue;
-                }
-
-                size
-            }
-            Err(_) => {
-                eprintln!("Некорректное число. Попробуйте еще раз.");
-                continue;
-            }
-        };
-
-        break size;
-        // let size = size.parse::<u32>().is_ok_and(|size| size > 0);
-    };
-
-    size
 }
