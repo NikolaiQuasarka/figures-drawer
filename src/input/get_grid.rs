8b8dc2a300e4f8a6bbc282dba766b6e3d615b924
nikolai <nikolaiquasarka@gmail.com>
2026-04-20 17:09:22 +0300

    Нужно увеличивать на 2, так как рамка занимает 2 доп ячейки

--- src/input/get_grid.rs
+++ src/input/get_grid.rs
@@ -7,7 +7,7 @@ pub fn get_grid() -> grid::Grid {
     loop {
         let grid_size = get_grid_size();
         // Добавляем 1 для рамки
-        let grid_size = basic::Size(grid_size.0 + 1, grid_size.1 + 1);
+        let grid_size = basic::Size(grid_size.0 + 2, grid_size.1 + 2);
 
         let Ok(grid) = grid::Grid::from(grid_size) else {
             eprint!("Try again");
